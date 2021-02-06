cd1fb21df7bd6a6eaf493e35e76fa43a709a62ac
Vitor Hugo <vhftavares21@gmail.com>
2021-02-06 14:45:13 +0000

    tutorial done

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const about = () => {
+  return (
+    <>
+      <Head>
+      <title>Ninja List | About</title>
+      <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi exercitationem molestias id, iure adipisci tempora dolore culpa sed impedit temporibus quam veniam, dolorem magnam ea non officiis necessitatibus cupiditate itaque!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea qui sequi saepe minus illum fugit exercitationem repellat numquam facere, harum ratione corrupti officiis, totam quaerat? Cum ipsum mollitia nisi natus!</p>
+      </div>
+    </>
+  );
+}
+
+export default about;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,20 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"/>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+      <div className={styles.title}>
+        <h1>Homepage</h1>
+        <p className={styles.text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi exercitationem molestias id, iure adipisci tempora dolore culpa sed impedit temporibus quam veniam, dolorem magnam ea non officiis necessitatibus cupiditate itaque!</p>
+        <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea qui sequi saepe minus illum fugit exercitationem repellat numquam facere, harum ratione corrupti officiis, totam quaerat? Cum ipsum mollitia nisi natus!</p>
+        <Link href="/ninjas"><a className={styles.btn}>See Ninja Listing</a></Link>
+      </div>
+    </>
+  );
 }
